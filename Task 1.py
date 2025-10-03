306b6187dbb903f1679c88ff9481be60b2449179
Kumar Nitin <nitin12073@gmail.com>
2025-10-03 16:54:54 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,10 @@
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+sum = a + b
+sub = a - b
+mul = a * b
+div = a / b
+print("Addition :", sum)
+print("Subtraction :", sub)
+print("Multiplication :", mul)
+print("Division :", div)
